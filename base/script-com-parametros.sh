2e8929b8d65d156823954a9b0cc8be05a59c53f9
walterpaulo <walter0paulo@hotmail.com>
2022-04-06 06:09:02 -0300

    refactor: code

---
+++ base/script-com-parametros.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Modo 1
+# echo "O primeiro parametro $1"
+# echo "O segundo parametro $2"
+# echo "O terceiro parametro $3"
+
+# Modo 2
+primeiro=$1
+segundo=$2
+terceiro=$3
+
+
+echo "O primeiro parametro $primeiro"
+echo "O segundo parametro $segundo"
+echo "O terceiro parametro $terceiro"
+
+
+
