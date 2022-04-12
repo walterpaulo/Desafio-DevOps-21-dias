7d4f6de43a65d6908bf1b2fde797b611a57068e9
walterpaulo <walter0paulo@hotmail.com>
2022-04-12 06:15:57 -0300

    feat: added subnet b1 and c1

--- base/terraform/example/8-vpc/1/subnets.tf
+++ base/terraform/example/8-vpc/1/subnets.tf
@@ -8,3 +8,23 @@ resource "aws_subnet" "subnet_a1" {
     }
   
 }
+
+resource "aws_subnet" "subnet_b1" {
+  vpc_id = aws_vpc.vpc_desafio.id
+  cidr_block = "10.0.128.0/18"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "subnet-b1"
+  }
+}
+
+resource "aws_subnet" "subnet_c1" {
+  vpc_id = aws_vpc.vpc_desafio.id
+  cidr_block = "10.0.192.0/18"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    "Name" = "subnet-c1"
+  }
+}
