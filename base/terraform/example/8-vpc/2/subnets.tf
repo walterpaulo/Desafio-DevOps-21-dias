eca6655eef0b398def7b084afa6673d811c1922e
walterpaulo <walter0paulo@hotmail.com>
2022-04-13 07:30:29 -0300

    fix: formatting

--- base/terraform/example/8-vpc/2/subnets.tf
+++ base/terraform/example/8-vpc/2/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnets" {
-  count          = length(keys(var.subnets))
+  count             = length(keys(var.subnets))
   vpc_id            = aws_vpc.vpc_desafio.id
   cidr_block        = var.subnets[keys(var.subnets)[count.index]].cdir
   availability_zone = var.subnets[keys(var.subnets)[count.index]].zona
