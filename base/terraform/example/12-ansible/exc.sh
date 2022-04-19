eb2513ec428e422e1a3985ec70d1a5703870e3ff
walterpaulo <walter0paulo@hotmail.com>
2022-04-18 22:12:08 -0300

    fix: rename host

--- base/terraform/example/12-ansible/exc.sh
+++ base/terraform/example/12-ansible/exc.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-cd terrafrom/
+cd terraform/
 terraform init
 terraform apply -auto-approve
 
-ANSILBLE_RETORNO=$(terraform output)
-HOST=(${ANSILBLE_RETORNO//=/ }) # terraform output public_dns
-DNS=${DNS[1]}
+#ANSILBLE_RETORNO=$(terraform output)
+#HOST=(${ANSILBLE_RETORNO//=/ }) # terraform output public_dns
 
-cd ../1
+ANSIBLE_RETORNO=$(terraform output)
+DNS=(${ANSIBLE_RETORNO//=/ })
+DNS=${DNS[1]}
+DNS=$(echo $DNS | sed -e "s/\"//g")
+
+# DNS=$(terraform output public_dns) # Rafa mais reduzido
+
+cd ../1/terraform
 
 echo "
-    [maquina-ansible]
-    $DNS
+[maquina-ansible]
+$DNS
 " > hosts
 
- ansible -i hosts provisionar.yml -u ubuntu --private-key $HOME/Desafio-DevOps-21-dias/base/terraform/example/providers/ssh/id_rsa
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ../../../providers/ssh/id_rsa
 
+# ssh -i ../../../providers/ssh/id_rsa ubuntu@$DNS -o ServerAliveInterval=60
