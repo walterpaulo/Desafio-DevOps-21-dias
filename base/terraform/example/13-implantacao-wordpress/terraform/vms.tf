3f4083dc452135863a1c602b95699b896e724c74
walterpaulo <walter0paulo@hotmail.com>
2022-04-20 15:30:09 -0300

    fix: modified instanace type

--- base/terraform/example/13-implantacao-wordpress/terraform/vms.tf
+++ base/terraform/example/13-implantacao-wordpress/terraform/vms.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm" {
   count         = 1
   ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.nano"
+  instance_type = "t2.medium"
   key_name      = aws_key_pair.chave_ssh_desafio.key_name
   vpc_security_group_ids = [
     aws_security_group.libera_ssh.id
